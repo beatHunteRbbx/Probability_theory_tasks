1584a9a7f353b30485a2fe07ad226a0aa89474b7
Nikita Sherepa <43075924+beatHunteRbbx@users.noreply.github.com>
2020-05-16 19:44:21 +0300

    исправил ошибки

--- src/Task7_11.java
+++ src/Task7_11.java
@@ -10,72 +10,65 @@ public class Task7_11 {
      * Какова вероятность того, что в урне содержатся только белые шары, если шары другого цвета не извлекались.
      * */
 
-    private static int numberOfExperiments = 20;
+    private static int numberOfExperiments = 10;
     private static int numberOfSituations = 100000;
     private static ArrayList<String> listOfNames = new ArrayList<>();
     private static ArrayList<double[]> listOfArrays = new ArrayList<>();
 
     public static void solve() {
-
-        for (int i = 0; i < 2; i++) {
-            int n = getRandomIntegerBetweenRange(5, 20);
-            int k = getRandomIntegerBetweenRange(5, n);
+        for (int i = 0; i < 3; i++) {
+            int n = getRandomIntegerBetweenRange(1, 30);
+            int k = getRandomIntegerBetweenRange(1, n);
             calculate(n, k);
         }
-        calculate(  getRandomIntegerBetweenRange(5, 15),
-                    getRandomIntegerBetweenRange(15, 20));
         createChart();
-
-
     }
 
-    private static void calculate(int entryN, int entryK) {
-        int n = entryN;
-        int k = entryK;
+    private static void calculate(int n, int k) {
 
-        String chartName = "n=" + n + " k=" + k +"       ";
-        listOfNames.add(chartName);
-        int numberOfValidSituations = 0;
+        int numberOfValidSituations;
 
         ArrayList<Integer> trash = new ArrayList<>();
-
         ArrayList<Integer> extractedBallsList = new ArrayList<>();
         double[] arrProb = new double[numberOfExperiments];
 
-        for (int experiment = 0; experiment < numberOfExperiments; experiment++) {
-            trash.clear();
-            n = entryN;
-            int rnd = getRandomIntegerBetweenRange(1, n);
-            for (int i = 0; i < rnd; i++) {
-                trash.add(0);    // 0 - белый шар
-                n--;
-            }
-            for (int i = 0; i < n; i++) trash.add(1);  // 1 - шары других цветов (не белые)
+        //заполняем урну:
+        //  0 - шар белого цвета
+        //  1 - шар не белого цвета
+        for (int i = 0; i < n; i++) trash.add(getRandomIntegerBetweenRange(0,1));
 
+        for (int experiment = 0; experiment < numberOfExperiments; experiment++) {
             numberOfValidSituations = 0;
             Collections.shuffle(trash);
-            extractedBallsList.clear();
             for (int situation = 0; situation < numberOfSituations; situation++) {
                 extractedBallsList.clear();
-                Collections.shuffle(trash);
                 for (int i = 0; i < k; i++) {
-                    int rndNumb = getRandomIntegerBetweenRange(0, entryN - 1);
+                    int rndNumb = getRandomIntegerBetweenRange(0, trash.size() - 1);
                     Integer ball = trash.get(rndNumb);
                     extractedBallsList.add(ball);
                 }
                 if (!extractedBallsList.contains(1)) numberOfValidSituations++;
             }
             arrProb[experiment] = (double) numberOfValidSituations / numberOfSituations;
-            System.out.println("Exp " + experiment + ": prob = " + arrProb[experiment]);
         }
-        listOfArrays.add(arrProb);
 
+        System.out.println("trash: "+ trash);
         double sum = 0;
         for (double prob : arrProb) {
             sum += prob;
         }
+        listOfNames.add("n=" + n + " k=" + k +"  средн.вер.=" + sum / (double) arrProb.length);
         System.out.println("avr. prob = " + sum / (double) arrProb.length);
+
+        //формульный ответ (аналитический)
+        sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Math.pow(i + 1, k);
+        }
+        System.out.println("formula=" + Math.pow(n,k) / sum);
         System.out.println();
+
+        listOfArrays.add(arrProb);
     }
 
     private static int getRandomIntegerBetweenRange(int min, int max){
