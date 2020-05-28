088681063b4f2f3e1ff91916ca318b98cdfba7cb
Nikita Sherepa <43075924+beatHunteRbbx@users.noreply.github.com>
2020-05-28 20:39:12 +0300

    исправил ошибки

--- src/Task12_17.java
+++ src/Task12_17.java
@@ -47,10 +47,6 @@ public class Task12_17 {
         listOfNames.add("v = " + v);
     }
 
-    private static int getRandomIntegerBetweenRange(int min, int max){
-        return (int) (Math.random()*((max-min)+1))+min;
-    }
-
     private static double getRandomDoubleBetweenRange(double min, double max){
         return (Math.random()*((max-min)))+min;
     }
